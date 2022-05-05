36a22f0116b775ae8c099872aaf11084e222f20b
patrickmcquay <60482558+patrickmcquay@users.noreply.github.com>
2022-05-05 12:57:33 -0400

    minor change to screen opening to be nicer to different screens, remove unneeded comment

--- rocotest.ino
+++ rocotest.ino
@@ -39,7 +39,7 @@ void setup() {
   Serial.begin(9600);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
